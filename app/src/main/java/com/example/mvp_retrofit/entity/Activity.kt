085737a99f11d964aa859bc7c652775e308479b4
krasnovss91 <krasnovstanislav91@gmail.com>
2022-03-25 10:54:32 +0400

    mvp_3

---
+++ app/src/main/java/com/example/mvp_retrofit/entity/Activity.kt
@@ -0,0 +1,4 @@
+package com.example.mvp_retrofit.entity
+
+class Activity {
+}
--- app/src/main/java/com/example/mvp_retrofit/view/MainActivity.kt
+++ app/src/main/java/com/example/mvp_retrofit/view/MainActivity.kt
@@ -4,8 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.mvp_retrofit.R
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity() {//activity не должно ничего знать о преобразованиях
+    override fun onCreate(savedInstanceState: Bundle?) {//дёргает методы presenter, все преобразования там
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
