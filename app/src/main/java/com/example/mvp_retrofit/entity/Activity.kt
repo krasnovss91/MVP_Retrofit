9bc268edb7449f20fb0aa3f77526c3f2eaf51946
krasnovss91 <krasnovstanislav91@gmail.com>
2022-03-26 11:06:46 +0400

    entity_1

--- app/src/main/java/com/example/mvp_retrofit/entity/Activity.kt
+++ app/src/main/java/com/example/mvp_retrofit/entity/Activity.kt
@@ -1,8 +1,14 @@
 package com.example.mvp_retrofit.entity
 
-class Activity {
-}
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 
-class ActivityViewState{
+@JsonClass(generateAdapter = true)
+data class Activity (
+    @Json(name = "activity") val activity: String
+        )
 
-}
+@JsonClass(generateAdapter = true)
+data class ActivityViewState(
+    @Json(name = "activity") val activity: String
+)
