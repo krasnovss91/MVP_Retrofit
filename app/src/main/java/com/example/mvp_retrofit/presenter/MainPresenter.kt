31e7e1ac5c64f00a6189a0c2aebc49ff2762afa5
krasnovss91 <krasnovstanislav91@gmail.com>
2022-03-26 11:59:59 +0400

    networkModule_1

--- app/src/main/java/com/example/mvp_retrofit/presenter/MainPresenter.kt
+++ app/src/main/java/com/example/mvp_retrofit/presenter/MainPresenter.kt
@@ -1,4 +1,13 @@
 package com.example.mvp_retrofit.presenter
 
-class MainPresenter {
+import com.example.mvp_retrofit.Contract
+
+class MainPresenter : Contract.Presenter {
+    override fun load() {
+        TODO("Not yet implemented")
+    }
+
+    override fun reload() {
+        TODO("Not yet implemented")
+    }
 }
--- app/src/main/java/com/example/mvp_retrofit/view/MainActivity.kt
+++ app/src/main/java/com/example/mvp_retrofit/view/MainActivity.kt
@@ -2,11 +2,45 @@ package com.example.mvp_retrofit.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mvp_retrofit.Contract
 import com.example.mvp_retrofit.R
+import com.example.mvp_retrofit.entity.ActivityViewState
 
-class MainActivity : AppCompatActivity() {//activity не должно ничего знать о преобразованиях
+class MainActivity : AppCompatActivity(), Contract.Viev {//activity не должно ничего знать о преобразованиях
     override fun onCreate(savedInstanceState: Bundle?) {//дёргает методы presenter, все преобразования там
         super.onCreate(savedInstanceState)//в сеть идут методы repository, которые вызывают методы presenter
         setContentView(R.layout.activity_main)
+    }
+
+    override fun showProgress() {
+        TODO("Not yet implemented")
+    }
+
+    override fun hideProgress() {
+        TODO("Not yet implemented")
+    }
+
+    override fun showError() {
+        TODO("Not yet implemented")
+    }
+
+    override fun showError(show: Boolean) {
+        TODO("Not yet implemented")
+    }
+
+    override fun hideError() {
+        TODO("Not yet implemented")
+    }
+
+    override fun hideContent() {
+        TODO("Not yet implemented")
+    }
+
+    override fun setContent(content: List<ActivityViewState>) {
+        TODO("Not yet implemented")
+    }
+
+    override fun showContent(show: Boolean) {
+        TODO("Not yet implemented")
     }
 }
