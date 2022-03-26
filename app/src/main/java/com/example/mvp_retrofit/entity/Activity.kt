2a15f36f796e2920d1cd0de0afc45270af49b673
krasnovss91 <krasnovstanislav91@gmail.com>
2022-03-26 11:21:02 +0400

    entity_2

--- app/src/main/java/com/example/mvp_retrofit/entity/Activity.kt
+++ app/src/main/java/com/example/mvp_retrofit/entity/Activity.kt
@@ -4,11 +4,23 @@ import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
-data class Activity (
-    @Json(name = "activity") val activity: String
-        )
+data class Activity(
+    @Json(name = "activity") val activity: String,
+    @Json(name = "type") val type: String,
+    @Json(name = "participants") val participants: Int,
+    @Json(name = "price") val price: Int,
+    @Json(name = "link") val link: String,
+    @Json(name = "key") val key: String,
+    @Json(name = "accessibility") val accessibility: Int
+)
 
 @JsonClass(generateAdapter = true)
 data class ActivityViewState(
-    @Json(name = "activity") val activity: String
+    @Json(name = "activity") val activity: String,
+    @Json(name = "type") val type: String,
+    @Json(name = "participants") val participants: Int,
+    @Json(name = "price") val price: Int,
+    @Json(name = "link") val link: String,
+    @Json(name = "key") val key: String,
+    @Json(name = "accessibility") val accessibility: Int
 )
