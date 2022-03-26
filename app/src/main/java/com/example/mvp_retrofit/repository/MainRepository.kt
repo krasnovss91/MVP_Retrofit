4acc593ff811f3ccc3500d48e82c4b88b8a718b5
krasnovss91 <krasnovstanislav91@gmail.com>
2022-03-26 11:48:22 +0400

    networkModule

--- app/src/main/java/com/example/mvp_retrofit/repository/MainRepository.kt
+++ app/src/main/java/com/example/mvp_retrofit/repository/MainRepository.kt
@@ -1,4 +1,34 @@
 package com.example.mvp_retrofit.repository
 
-class MainRepository {
+import com.example.mvp_retrofit.Contract
+import com.example.mvp_retrofit.entity.Activity
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
+
+class MainRepository : Contract.Repository{
+    override fun load(): MutableList<Activity> {
+        TODO("Not yet implemented")
+    }
+
+    override fun reload() {
+        TODO("Not yet implemented")
+    }
+
+    object NetworkModule{
+        private val client = OkHttpClient().newBuilder()
+            .readTimeout(30000,TimeUnit.MILLISECONDS)
+            .connectTimeout(30000, TimeUnit.MILLISECONDS)
+            .writeTimeout(30000, TimeUnit.MILLISECONDS)
+            .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .build()
+
+        private val retrofit = Retrofit.Builder()
+            .baseUrl("https://www.boredapi.com/api/")
+            .addConverterFactory(MoshiConverterFactory.create())
+            .client(client)
+            .build()
+    }
 }
