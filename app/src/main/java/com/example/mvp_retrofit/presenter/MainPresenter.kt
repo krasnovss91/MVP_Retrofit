ddd94c422e86f9120a98753ac8f363433cbed2a0
krasnovss91 <krasnovstanislav91@gmail.com>
2022-03-27 18:02:46 +0400

    presenter_4

--- app/src/main/java/com/example/mvp_retrofit/presenter/MainPresenter.kt
+++ app/src/main/java/com/example/mvp_retrofit/presenter/MainPresenter.kt
@@ -5,9 +5,10 @@ import com.example.mvp_retrofit.entity.ActivityViewState
 
 class MainPresenter(
 
-private val view : Contract.Viev,
-private val repository: Contract.Repository
-) : Contract.Presenter {//здесь смапить Activity к ActivityViewState
+    private val view: Contract.Viev,
+    private val repository: Contract.Repository
+) : Contract.Presenter {
+    //здесь смапить Activity к ActivityViewState
     override fun load() {
 
         try {
@@ -27,10 +28,13 @@ private val repository: Contract.Repository
                     activity.accessibility
                 )
             }
+            view.setContent(acivitiesViewState)
+            view.hideProgress()
+            view.showContent(true)
 
-        } catch (e: Throwable){
-          view.showProgress()
-          view.showError()
+        } catch (e: Throwable) {
+            view.showProgress()
+            view.showError()
         }
 
     }
