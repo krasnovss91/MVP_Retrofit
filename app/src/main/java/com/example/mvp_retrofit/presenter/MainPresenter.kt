965fdc0da24075483fc8e8d31b5a6e26bebb853b
krasnovss91 <krasnovstanislav91@gmail.com>
2022-03-27 16:55:39 +0400

    presenter_1

--- app/src/main/java/com/example/mvp_retrofit/presenter/MainPresenter.kt
+++ app/src/main/java/com/example/mvp_retrofit/presenter/MainPresenter.kt
@@ -1,10 +1,56 @@
 package com.example.mvp_retrofit.presenter
 
 import com.example.mvp_retrofit.Contract
+import com.example.mvp_retrofit.repository.MainRepository
 
-class MainPresenter : Contract.Presenter {//здесь смапить Activity к ActivityViewState
+class MainPresenter(
+
+private val view : Contract.Viev,
+private val repository: Contract.Repository
+) : Contract.Presenter {//здесь смапить Activity к ActivityViewState
     override fun load() {
-        TODO("Not yet implemented")
+
+        try {
+            view.hideContent()
+            view.showProgress()
+
+            val activities = repository.load()
+
+
+
+        } catch (e: Throwable){
+          view.showProgress()
+          view.showError()
+        }
+       /*
+               try {
+
+            view.hideContent()
+
+            view.showProgress()
+
+            val products = repository.load()
+
+            val productsViewState: List<ProductViewState> = products.map { product ->
+                ProductViewState(
+                    product.avatar,
+                    product.name,
+                    product.producer,
+                    product.cost,
+                    product.id,
+                    product.offsetDateTime
+                )
+            }
+            view.setContent(productsViewState)
+            view.hideProgress()
+            view.showContent(true)
+        } catch (e: Throwable) {
+
+            view.showProgress()
+
+            view.showError()
+        }
+        */
     }
 
     override fun reload() {
