9ffbb78c603c3a4e9a390cf256f952f414f4a69f
krasnovss91 <krasnovstanislav91@gmail.com>
2022-03-28 12:04:30 +0400

    view_7

--- app/src/main/java/com/example/mvp_retrofit/view/MainActivity.kt
+++ app/src/main/java/com/example/mvp_retrofit/view/MainActivity.kt
@@ -6,15 +6,19 @@ import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mvp_retrofit.Contract
 import com.example.mvp_retrofit.R
 import com.example.mvp_retrofit.entity.ActivityViewState
 import com.example.mvp_retrofit.presenter.MainPresenter
 import com.example.mvp_retrofit.repository.MainRepository
+import com.example.mvp_retrofit.view.adapter.RequestAdapter
 
 class MainActivity : AppCompatActivity(),
     Contract.Viev {//activity не должно ничего знать о преобразованиях
+
+    private val adapter = RequestAdapter()
 
     lateinit var requestList: RecyclerView
     lateinit var requestInfo: TextView
@@ -34,7 +38,28 @@ class MainActivity : AppCompatActivity(),
     override fun onCreate(savedInstanceState: Bundle?) {//дёргает методы presenter, все преобразования там
         super.onCreate(savedInstanceState)//в сеть идут методы repository, которые вызывают методы presenter
         setContentView(R.layout.activity_main)
+
+        progress = findViewById(R.id.progress)
+        errorTitle = findViewById(R.id.errorTitle)
+        reloadButton = findViewById(R.id.reload)
+
+        presenter.load()
+
+        requestButton.setOnClickListener {
+
+        }
+
+        reloadButton.setOnClickListener {
+            presenter.reload()
+        }
     }
+
+   private fun SetUpViews(){
+      requestList = findViewById<RecyclerView>(R.id.requestList).apply {
+         layoutManager = LinearLayoutManager(this@MainActivity)
+         adapter = this@MainActivity.adapter
+      }
+   }
 
     override fun showProgress() {
         progress.isVisible = true
