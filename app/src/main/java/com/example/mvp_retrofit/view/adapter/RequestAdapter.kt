b0b1be8d6253d8da72d6e7a92b499a26efdaa427
krasnovss91 <krasnovstanislav91@gmail.com>
2022-03-28 12:42:52 +0400

    adapter_1

--- app/src/main/java/com/example/mvp_retrofit/view/adapter/RequestAdapter.kt
+++ app/src/main/java/com/example/mvp_retrofit/view/adapter/RequestAdapter.kt
@@ -1,4 +1,12 @@
 package com.example.mvp_retrofit.view.adapter
 
+import com.example.mvp_retrofit.entity.ActivityViewState
+
 class RequestAdapter {
+
+    private var requestList = emptyList<ActivityViewState>()
+
+    fun setRequests(requestsViewState: List<ActivityViewState>?){
+
+    }
 }
