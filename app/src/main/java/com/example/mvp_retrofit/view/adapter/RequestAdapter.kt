5008b88f2cc7c796215ad02b96e6ee1b9f8aac59
krasnovss91 <krasnovstanislav91@gmail.com>
2022-04-05 12:05:48 +0400

    adapter_111

--- app/src/main/java/com/example/mvp_retrofit/view/adapter/RequestAdapter.kt
+++ app/src/main/java/com/example/mvp_retrofit/view/adapter/RequestAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.mvp_retrofit.view.adapter
 
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mvp_retrofit.entity.ActivityViewState
 
@@ -21,5 +22,17 @@ class RequestAdapter: RecyclerView.Adapter<RequestAdapter.MyViewHolder>() {
         }
 
 
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
     }
 }
